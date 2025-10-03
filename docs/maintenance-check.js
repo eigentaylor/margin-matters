53137554cf701e13f40627ca00dbb0ac09bc8df0
eigentaylor <tayloreigenfisher@gmail.com>
2025-10-03 01:38:24 -0700

    Add maintenance-check to head

--- docs/maintenance-check.js
+++ docs/maintenance-check.js
@@ -1,8 +1,8 @@
 // Maintenance mode check
 // This script checks if maintenance mode is enabled and redirects to maintenance page if needed
 (function() {
-  const MAINTENANCE_ENABLED = true;
-  const MAINTENANCE_PASSWORD_HASH = '-1634889951';
+  const MAINTENANCE_ENABLED = false;
+  const MAINTENANCE_PASSWORD_HASH = '573157533';
   
   function simpleHash(str) {
     let hash = 0;
