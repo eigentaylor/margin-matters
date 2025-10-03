1b16ca18ad80b11f5c36d3f45386d8da5d130623
eigentaylor <tayloreigenfisher@gmail.com>
2025-10-03 00:48:09 -0700

    Update config.py

--- site_builder/config.py
+++ site_builder/config.py
@@ -17,7 +17,7 @@ ME_NE_STATES = {"ME-AL", "NE-AL"}
 # Set MAINTENANCE_MODE to True to enable maintenance mode
 # When enabled, users will be redirected to maintenance.html unless they enter the correct password
 MAINTENANCE_MODE = True
-MAINTENANCE_PASSWORD = "ericadams2028"  # Change this to your desired password
+MAINTENANCE_PASSWORD = "ericadams2028"  # Oh no! Don't tell anyone!
 
 # timestamp used in footers (UTC at build time)
 LAST_UPDATED = datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d %H:%M UTC")
